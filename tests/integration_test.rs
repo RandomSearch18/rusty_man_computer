3dc8e645bf6da7f6af6a392bedb2827437245bbf
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-02-25 22:08:19 +0000

    Add more integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -8,6 +8,7 @@ fn test_ascii_program() {
         load_ram_file_path: Some(PathBuf::from("demos/ascii.bin")),
         print_computer_state: false,
         print_raw_output: false,
+        input: None,
     });
     computer
         .initialize_ram_from_file()
@@ -18,4 +19,43 @@ fn test_ascii_program() {
         output,
         r##" !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"##
     );
+}
+
+#[test]
+fn test_add_program() {
+    let number_1 = 3;
+    let number_2 = -5;
+    let expected_output = number_1 + number_2;
+    let mut computer = Computer::new(Config {
+        load_ram_file_path: Some(PathBuf::from("demos/add.bin")),
+        print_computer_state: false,
+        print_raw_output: false,
+        input: Some(vec![number_1, number_2]),
+    });
+    computer
+        .initialize_ram_from_file()
+        .expect("Failed to initialize RAM from file");
+    computer.run();
+    assert_eq!(computer.output.read_all(), expected_output.to_string());
+}
+
+#[test]
+fn test_add_subtract_program() {
+    let number_1 = 10;
+    let number_2 = 11;
+    let expected_sum = number_1 + number_2;
+    let number_3 = 100;
+    let expected_difference = number_3 - number_1;
+    let mut computer = Computer::new(Config {
+        load_ram_file_path: Some(PathBuf::from("demos/add-subtract.bin")),
+        print_computer_state: false,
+        print_raw_output: false,
+        input: Some(vec![number_1, number_2, number_3]),
+    });
+    computer
+        .initialize_ram_from_file()
+        .expect("Failed to initialize RAM from file");
+    computer.run();
+    let expected_output = [expected_sum.to_string(), expected_difference.to_string()].join("\n");
+    assert_eq!(computer.output.read_all(), expected_output);
 }
