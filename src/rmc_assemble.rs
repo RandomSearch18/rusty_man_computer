7906c8a36218d865001f9eae763f3f7615e9bb17
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-03-04 23:42:07 +0000

    woah that's a lot of writing I've just done

--- src/rmc_assemble.rs
+++ src/rmc_assemble.rs
@@ -1,6 +1,133 @@
 use rusty_man_computer::value::Value;
 
+enum Opcode {
+    HLT,
+    ADD,
+    SUB,
+    STA,
+    LDA,
+    BRA,
+    BRZ,
+    BRP,
+    INP,
+    OUT,
+    OTC,
+    DAT,
+}
+
+enum Operand {
+    Value(Value),
+    Label(String),
+}
+
+enum Line {
+    Empty(),
+    Instruction {
+        label: Option<String>,
+        opcode: Opcode,
+        operand: Option<Operand>,
+    },
+}
+
+enum ParseError {
+    InvalidOpcode(String),
+    OperandOutOfRange(i16),
+}
+
+enum State {
+    Unknown,
+}
+
+fn parse_opcode(string: &str) -> Option<Opcode> {
+    match string {
+        "HLT" => Some(Opcode::HLT),
+        "ADD" => Some(Opcode::ADD),
+        "SUB" => Some(Opcode::SUB),
+        "STA" => Some(Opcode::STA),
+        "LDA" => Some(Opcode::LDA),
+        "BRA" => Some(Opcode::BRA),
+        "BRZ" => Some(Opcode::BRZ),
+        "BRP" => Some(Opcode::BRP),
+        "INP" => Some(Opcode::INP),
+        "OUT" => Some(Opcode::OUT),
+        "OTC" => Some(Opcode::OTC),
+        "DAT" => Some(Opcode::DAT),
+        _ => None,
+    }
+}
+
 fn assemble(program: &str) -> Vec<Value> {
+    let mut state = State::Unknown;
+    let parsed: Vec<Result<Line, ParseError>> = program
+        .lines()
+        .map(|line| {
+            // for char in line.chars() {
+            //     match state {
+            //         State::Unknown => {
+            //             if char.is_whitespace() {
+            //                 continue;
+            //             }
+            //             if char.is_alphabetic() {
+            //                 state = State::smth
+            //             }
+            //         }
+            //     }
+            // }
+            let line = line.trim();
+            if line.is_empty() || line.starts_with("//") {
+                return Ok(Line::Empty());
+            }
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            if parts.len() == 0 {
+                return Ok(Line::Empty());
+            }
+            // If the first part isn't a valid opcode, use it as a label
+            let first_part_as_opcode = parse_opcode(parts[0]);
+            let label = match first_part_as_opcode {
+                Some(_) => None,
+                None => Some(parts[0].to_string()),
+            };
+            // If we've already found a valid opcode in the first part, use it
+            // Otherwise, try parsing the second part as an opcode
+            let opcode = match first_part_as_opcode {
+                Some(opcode) => opcode,
+                None => {
+                    let string = match parts.get(1) {
+                        Some(string) => string,
+                        // This means there's only one part: there's nothing to label, so it's just an invalid opcode
+                        None => return Err(ParseError::InvalidOpcode(parts[0].to_string())),
+                    };
+                    match parse_opcode(string) {
+                        Some(opcode) => opcode,
+                        None => return Err(ParseError::InvalidOpcode(string.to_string())),
+                    }
+                }
+            };
+            let operand_part = if label.is_some() {
+                parts.get(2)
+            } else {
+                parts.get(1)
+            };
+            // If the operand is a valid number, parse it as a Value
+            // Else, consider it a label
+            let operand = match operand_part {
+                Some(string) => match string.parse::<i16>() {
+                    Ok(value) => Some(Operand::Value(
+                        // If the number doesn't fit within a Value, return an OperandOutOfRange error
+                        Value::new(value).map_err(|_| ParseError::OperandOutOfRange(value))?,
+                    )),
+                    Err(_) => Some(Operand::Label(string.to_string())),
+                },
+                None => None,
+            };
+            Ok(Line::Instruction {
+                label,
+                opcode,
+                operand,
+            })
+        })
+        .collect();
+
     vec![]
 }
 
